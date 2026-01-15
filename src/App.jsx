1389f77193557e3ed9e0e5509d14dac0b83fd9a8
auroraengelgeer <auroraengelgeer@hotmail.com>
2026-01-15 17:22:32 +0100

    added about and FAQ pages with routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Profile from "./pages/Profile/Profile.jsx";
 import SignIn from "./pages/SignIn/SignIn.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
 import TravelDetail from "./pages/TravelDetail/TravelDetail.jsx";
+import About from "./pages/About/About.jsx";
+import Faq from "./pages/Faq/Faq.jsx";
+
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
   return (
     <>
       <Routes>
+          <Route path="/over-ons" element={<About />} />
+          <Route path="/faq" element={<Faq />} />
+
           <Route path="/" element={<Home />} />
           <Route path="/reizen" element={<TravelOverview />} />
           <Route path="/reizen/:travelId" element={<TravelDetail />} />
