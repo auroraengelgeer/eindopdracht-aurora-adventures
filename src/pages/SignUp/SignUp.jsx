158d1157dd35930ce4a81985e4a38e080f3ec6c5
auroraengelgeer <auroraengelgeer@hotmail.com>
2026-02-10 23:31:51 +0100

    DRY: SignUp uses shared AuthLayout

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createProfile } from "../../api/profiles";
 import { createUser } from "../../api/users";
+import AuthLayout from "../../components/AuthLayout/AuthLayout";
 import "./SignUp.css";
 
 
@@ -55,88 +56,77 @@ export default function SignUp() {
 
 
     return (
-        <div className="auth-page">
-            {/* LINKS */}
-            <section className="auth-left auth-left--signup">
-                <div className="auth-left-overlay">
-                    <h1>Maak je account aan</h1>
-                    <p>
-                        Sla je gegevens op, beheer boekingen en rond je reservering sneller
-                        af.
-                    </p>
+        <AuthLayout
+            leftClassName="auth-left auth-left--signup"
+            leftTitle="Maak je account aan"
+            leftText="Sla je gegevens op, beheer boekingen en rond je reservering sneller af."
+            rightInnerClassName="auth-form-wrapper"
+        >
+            <h2 className="auth-title">Registreren</h2>
+
+            <form className="auth-form" onSubmit={handleSubmit}>
+                <div className="grid-2">
+                    <div className="field">
+                        <label htmlFor="firstName">Voornaam</label>
+                        <input
+                            id="firstName"
+                            type="text"
+                            placeholder="Jan"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="lastName">Achternaam</label>
+                        <input
+                            id="lastName"
+                            type="text"
+                            placeholder="de Vries"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            required
+                        />
+                    </div>
                 </div>
-            </section>
 
-            {/* RECHTS */}
-            <section className="auth-right">
-                <div className="auth-form-wrapper">
-                    <h2 className="auth-title">Registreren</h2>
-
-                    <form className="auth-form" onSubmit={handleSubmit}>
-                        <div className="grid-2">
-                            <div className="field">
-                                <label htmlFor="firstName">Voornaam</label>
-                                <input
-                                    id="firstName"
-                                    type="text"
-                                    placeholder="Jan"
-                                    value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}
-                                    required
-                                />
-                            </div>
-
-                            <div className="field">
-                                <label htmlFor="lastName">Achternaam</label>
-                                <input
-                                    id="lastName"
-                                    type="text"
-                                    placeholder="de Vries"
-                                    value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div className="field">
-                            <label htmlFor="email">Email adres</label>
-                            <input
-                                id="email"
-                                type="email"
-                                placeholder="jij@voorbeeld.nl"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div className="field">
-                            <label htmlFor="password">Wachtwoord</label>
-                            <input
-                                id="password"
-                                type="password"
-                                placeholder="Minimaal 6 tekens"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                                minLength={6}
-                            />
-                        </div>
-
-                        <button className="button button-primary auth-submit" type="submit">
-                            Account aanmaken
-                        </button>
-                    </form>
-
-                    <p className="auth-bottom-text">
-                        Heb je al een account?{" "}
-                        <Link className="auth-link" to="/inloggen">
-                            Log in
-                        </Link>
-                    </p>
+                <div className="field">
+                    <label htmlFor="email">Email adres</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="jij@voorbeeld.nl"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
-            </section>
-        </div>
+
+                <div className="field">
+                    <label htmlFor="password">Wachtwoord</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Minimaal 6 tekens"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        minLength={6}
+                    />
+                </div>
+
+                <button className="button button-primary auth-submit" type="submit">
+                    Account aanmaken
+                </button>
+            </form>
+
+            <p className="auth-bottom-text">
+                Heb je al een account?{" "}
+                <Link className="auth-link" to="/inloggen">
+                    Log in
+                </Link>
+            </p>
+        </AuthLayout>
     );
 }
